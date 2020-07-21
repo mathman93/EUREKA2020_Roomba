1a79c311e120acbc661ca32186caf8a950732a99
tzakrzwski <zaktazzak@gmail.com>
2020-07-21 18:56:29 -0400

    f

---
+++ Plotter.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import os
+
+plt.ion()
+
+filenames = ['raspberrypi1_July 21, 2020, 22:13:53.csv',
+             'raspberrypi2_July 21, 2020, 22:13:58.csv']
+#filenames = ['Node1.csv']
+colors = ['r', 'b', 'g', 'c', 'm', 'y']
+i = 0
+for fm in filenames:
+    with open(os.path.join('Data_Files', fm), newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        x = []
+        y = []
+        for row in reader:
+            try:
+                x.append(float(row[0]))
+                y.append(float(row[1]))
+            except:
+                print('Help')
+            if row[3] == str(1):
+                plt.plot(float(row[0]), float(row[1]), marker='o')
+        plt.plot(x,y,colors[i])
+        i += 1
+
