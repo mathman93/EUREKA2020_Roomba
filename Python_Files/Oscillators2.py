a41ade609bb12d8a6c3a835fb0d6a54b1d52cc1e
TaylorC20 <tmcor2an@yahoo.com>
2020-07-21 16:30:44 -0400

    Print nodephase each loop Removed * 12

--- Python_Files/Oscillators2.py
+++ Python_Files/Oscillators2.py
@@ -26,7 +26,8 @@ while True:
         ## Increasing the phase value ##
         dt = time.time() - time1
         if dt > sendtimemin:
-            nodephase += dt * 12
+            nodephase += dt
+            print(nodephase)
 
         ## Reaching the Threshold and Sending Pulses ##
         if nodephase >= threshold:
