7907907213a2ba3a149adec434cd17e9b16de7dd
TaylorC20 <tmcor2an@yahoo.com>
2020-07-23 13:41:19 -0400

    First attempt to fix natural log function

--- Python_Files/Taylor_Mirollor_Strogatz_Oscillators.py
+++ Python_Files/Taylor_Mirollor_Strogatz_Oscillators.py
@@ -40,7 +40,7 @@ while True:
             message = Xbee.read(Xbee.inWaiting()).decode() # Read all data in
             print(message) # To see what the message is
             print(nodephase)
-            x = (1/2) * (np.log(1 + (e^2 - 1) * nodephase))
+            x = (1/2) * (log((1 + (e^2 - 1) * nodephase), e))
             if 0 < x <= 3.5242:
                 x -= epsilon
             if 3.524 < x < 3.8705:
