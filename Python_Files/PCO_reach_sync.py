abfac5fbe99bce5f1459fd980738199eb500fa8e
tzakrzwski <zaktazzak@gmail.com>
2020-07-23 17:08:57 -0400

    f

--- Python_Files/PCO_reach_sync.py
+++ Python_Files/PCO_reach_sync.py
@@ -11,9 +11,9 @@ from random import uniform
 LOG_PERIOD = .01 #Time between when to log data
 PERIOD = 2 #Time in seconds for each Ossilation
 HALF_PERIOD = PERIOD/2
-STRENGTH = .9 #Coe used with function to determine coupling strength?
+STRENGTH = .6 #Coe used with function to determine coupling strength?
 REFRACT = 0 #Time before listen more signals
-SYNC_THRESHOLD = .001 #Seconds between pulses that counts as syncronized
+SYNC_THRESHOLD = .005 #Seconds between pulses that counts as syncronized
 
 
 
