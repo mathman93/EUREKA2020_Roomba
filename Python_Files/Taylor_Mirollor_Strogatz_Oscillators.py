46673987da81bef263997105c0267a2870ddd8ab
TaylorC20 <tmcor2an@yahoo.com>
2020-07-23 13:48:14 -0400

    Fixed powers

--- Python_Files/Taylor_Mirollor_Strogatz_Oscillators.py
+++ Python_Files/Taylor_Mirollor_Strogatz_Oscillators.py
@@ -40,12 +40,12 @@ while True:
             message = Xbee.read(Xbee.inWaiting()).decode() # Read all data in
             print(message) # To see what the message is
             print(nodephase)
-            x = (1/2) * (log((1 + (e^2 - 1) * nodephase), e))
+            x = (1/2) * (log((1 + (e**2 - 1) * nodephase), e))
             if 0 < x <= 3.5242:
                 x -= epsilon
             if 3.524 < x < 3.8705:
                 x += epsilon
-            NodephasePlus = (e^(2 * x) - 1)/(e^2 - 1)
+            NodephasePlus = (e**(2 * x) - 1)/(e**2 - 1)
             time1 += NodephasePlus
 
     ## Keyboard Interupt ##
