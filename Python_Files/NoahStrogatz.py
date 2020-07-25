277467fd3b50b9c41a4914a452c571a8a8f30c6b
noahtav <67967658+noahtav@users.noreply.github.com>
2020-07-25 15:21:47 -0400

    Update NoahStrogatz.py

--- Python_Files/NoahStrogatz.py
+++ Python_Files/NoahStrogatz.py
@@ -1,14 +1,24 @@
 import serial
+import time
+import random
+from copy import copy
 
 global Xbee
 Xbee = serial.Serial('/dev/ttyUSB0', 115200)
 
 phase = random.randint(0,360)
 threshold = 360
-variable = 0
+previousTime = time.time()
+currentTime = time.time()
 
 while True:
-	try: 
+
+	try:
+	
+		previousTime = copy(currentTime)
+		currentTime = time.time()
+		timeDifference = currentTime - previousTime
+		phase += timeDifference * 60
 
 		if phase >= threshold:
 			phase = 0
@@ -16,11 +26,18 @@ while True:
 			Xbee.write(message.encode())
 			print("Pulse")
 
+
 		if Xbee.inWaiting() > 0:
 			message = Xbee.read(Xbee.inWaiting()).decode()
 			print(message)
-			var = phase * ?
-			var += ?
-			phase = var * ?
-			
+			if 0 <= phase <= 180:
+				phase -= phase
+			if 180 < phase <= threshold:
+				phase += (threshold - phase)
 
+
+	except KeyboardInterrupt:
+		print("Process Interrupted")
+		break
+
+Xbee.close()
