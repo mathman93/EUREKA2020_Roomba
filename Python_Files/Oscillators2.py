31fd1434c0b4df380420ddf130ca0b4f7f56611f
TaylorC20 <tmcor2an@yahoo.com>
2020-07-26 13:10:29 -0400

    Altered change in time1 when receiving pulse

--- Python_Files/Oscillators2.py
+++ Python_Files/Oscillators2.py
@@ -35,9 +35,9 @@ while True:
             print(message) # To see what the message is
             print(nodephase)
             if 0 < nodephase <= 180:
-                time1 += 5
+                time1 += nodephase/10
             if 180 < nodephase < threshold:
-                time1 -= 5
+                time1 -= nodephase/10
 
     ## Keyboard Interupt ##
     except KeyboardInterrupt:
