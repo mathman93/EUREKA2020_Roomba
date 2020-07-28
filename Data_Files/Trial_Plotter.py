9eeaf101ea75c24cf55a8e8427cc290bca0cf41c
tzakrzwski <zaktazzak@gmail.com>
2020-07-28 13:21:55 -0400

    redid plotter and changed up some stuff

--- Data_Files/Trial_Plotter.py
+++ Data_Files/Trial_Plotter.py
@@ -6,8 +6,9 @@ import glob
 
 plt.ioff()
 #Give file path for all the datas that want to plot
-rp1_path = os.path.join('raspberrypi1', 'raspberrypi1_Jul27_10AM10', 'Delay_Advance')
-rp2_path = os.path.join('raspberrypi2', 'raspberrypi2_Jul27_10AM10', 'Delay_Advance')
+rp1_path = os.path.join('raspberrypi1', 'raspberrypi1_905', 'Delay_Advance')
+rp2_path = os.path.join('raspberrypi2', 'raspberrypi2_905', 'Delay_Advance')
+rp3_path = os.path.join('raspberrypi3', 'raspberrypi3_905', 'Delay_Advance')
 #Manually drill down into each directory and create seperate lists IN ORDER of the data
 #I know should be better, but I need to get this done
 rp1_Files = []
@@ -21,6 +22,13 @@ rp2_Paths = []
 for file in glob.glob(rp2_path + '/*.csv'):
     rp2_Paths.append(file)
     rp2_Files.append(os.path.basename(file))
+rp3_Files = []
+rp3_Paths = []
+print(rp3_path)
+for file in glob.glob(rp3_path + '/*.csv'):
+    rp3_Paths.append(file)
+    rp3_Files.append(os.path.basename(file))
+print(rp2_Paths)
 
 ##rp1_Files = []
 ##rp1_Paths = []
@@ -41,7 +49,7 @@ for file in glob.glob(rp2_path + '/*.csv'):
 
 
 #Check to make sure all the same files
-if rp1_Files == rp2_Files:
+if rp1_Files == rp2_Files == rp3_Files:
     print('Matching files == good to go!!!')
 else:
     print('Files not match')
@@ -49,9 +57,9 @@ else:
 
 #Loop through all the files
 for j in range(len(rp1_Files)):
-    filenames = [rp1_Paths[j], rp2_Paths[j]]
+    filenames = [rp1_Paths[j], rp2_Paths[j], rp3_Paths[j]]
     print(filenames)
-    colors = ['r', 'b', 'g', 'c', 'm', 'y']
+    colors = ['r', 'b', 'y', 'c', 'm', 'g']
     i = 0
     for fm in filenames:
         with open(fm, newline='') as f:
--- Plotter.py
+++ Plotter.py
@@ -5,99 +5,137 @@ import os
 import glob
 
 plt.ioff()
-
-
-#Get file names
-##x = os.listdir('Data_Files')
-##print(x)
-##filenames = []
-##for name in x:
-##    n = os.path.join('Data_Files',name)
-##    if os.path.isdir(n):
-##        ans = input('Want to Draw Contents of '+ name + ' ?')
-##        if ans == 'y':
-##            for file in glob.glob("Data_Files/"+name+"/*.csv"):
-##                filenames.append(file)
-##        elif ans == 'q':
-##            break
-##        else:
-##            print('Not including')
-##    elif os.path.isfile(n):
-##        if os.path.splitext(name)[-1].lower() == '.csv':
-##            ans = input('Want to Draw ' + name + ' ?')
-##            if ans == 'y':
-##                filenames.append(n)
-##            elif ans == 'q':
-##                break
-##            else:
-##                print('Not including')
-
+old_way = False
 filenames = []
 
-ans = input('Get most recent? ')
-x = os.listdir('Data_Files')
-if ans == 'y':
-    #Find the most recent files in each non-y containing directory
-    for name in x:
-        possible_files = []
-        path = os.path.join('Data_Files',name)
-        #Check if directory
-        if os.path.isdir(path):
-            #Check if contains a y
-            if 'z' not in name:
-                #Look at all the files in the directory that are .csv
-                for file in glob.glob(path+"/**/*.csv", recursive=True):
-                    #print(file)
-                    possible_files.append(file)
-                #Then, cycle through and find the one with the largest number
-                greatest = '0'
-                great_path = None
-                for file in possible_files:
-                    #Very dirty, put should work
-                    fname = os.path.basename(file)
-                    num = []
-                    for char in fname:
-                        try:
-                            int(char) #check if number
-                            num.append(char)
-                            #print('new number ', char)
-                        except:
-                            continue
-                    q = ''.join(num)
-                    #print(q)
-                    if int(q) > int(greatest):
-                        greatest = int(q)
-                        great_path = file
-                filenames.append(great_path)
-    print(filenames)
-    ans = input('Are these the files you are looking for? ')
-if ans != 'y':
-    filenames = []
-    print('ok, lets do this differently')
-    print('pick you favorite files from each folder')
-    for name in x:
-        possible_files = []
-        path = os.path.join('Data_Files',name)
-        if os.path.isdir(path):
-            #Check if contains a y
-            if 'z' not in name:
-                for file in glob.glob(path+"/PCO_Data/*.csv"):
-                    possible_files.append(file)
-                #Choice which file from the list
-                print('Directory: ', name)
-                i = 0
-                for file in possible_files:
-                    print(i, ' - ', os.path.basename(file))
-                    i += 1
-                index = input('Choice index of the one you want ')
-                try:
-                    if index == 'n':
-                        print('skipping directory')
-                    elif 0 <= int(index) <= len(possible_files)-1:
-                        filenames.append(possible_files[int(index)])
-                except:
-                    print('Bad number, this is the end')
-                    exit()
+#Pull down files from the pis
+if input('Want to pull info down from pis? ') == 'y':
+    os.system('scp -r pi@192.168.1.14:PCO_Data Data_Files/raspberrypi1')
+    os.system('scp -r pi@192.168.1.15:PCO_Data Data_Files/raspberrypi2')
+    os.system('scp -r pi@192.168.1.17:PCO_Data Data_Files/raspberrypi3')
+#CHANGE SO THAT ALL MATCHING FILES HAVE SAME ENDINGS
+
+#Basicly, look at the PCO_data folder of an pi and determine when sufix file to
+#graph
+#Can opt out by typeing 'old' on first question
+x = input("Which pi's to look at? ")
+if x == 'old':
+    old_way = True
+elif x in ['1', '2', '3']:
+    #The directory to look at for all the files
+    path = os.path.join('Data_Files', 'raspberrypi' + x, 'PCO_Data')
+    #create list of sufixes
+    suffixes = []
+    for file in os.listdir(path):
+        suffixes.append(file.split('_')[-1])
+    suffixes.sort()
+    #Search for file suffixes either from index OR by name
+    while True:
+        if input('Search by suffix? ') == 'y':
+            #Searxh for suffix by name
+            x = input('Enter suffix: ')
+            if x in suffixes:
+                print('Found suffix, getting data and graphing')
+                file_suffix = x
+                break
+            else:
+                print('Invalid, try another suffix or search by index')
+        else:
+            #Choice which sufix from the list
+            i = 0
+            for suffix in suffixes:
+                print(i, ' - ', suffix)
+                i += 1
+            index = input('Choice index of the one you want ')
+            try:
+                if 0 <= int(index) <= len(possible_files)-1:
+                    print('Found suffix, getting data and graphing')
+                    file_suffix = suffixes[int(index)])
+                    break
+                else:
+                    print('Invalid, try another index or search by suffix')
+            except:
+                print('Invalid, try another index or search by suffix')
+
+    #Get the files and add path names to filenames
+    for pi_num in ['1', '2', '3']:
+        p = os.path.join('Data_Files', 'raspberrypi' + pi_num, 'PCO_Data',
+                         'raspberrypi' + pi_num + '_' + file_suffix)
+        if os.path.exists(p):
+            filenames.append(p)
+
+
+
+
+
+
+
+#Keep old functionality for odd cases
+if old_way == True:
+    ans = input('Get most recent? ')
+    x = os.listdir('Data_Files')
+    if ans == 'y':
+        #Find the most recent files in each non-y containing directory
+        for name in x:
+            possible_files = []
+            path = os.path.join('Data_Files',name)
+            #Check if directory
+            if os.path.isdir(path):
+                #Check if contains a y
+                if 'z' not in name:
+                    #Look at all the files in the directory that are .csv
+                    for file in glob.glob(path+"/**/*.csv", recursive=True):
+                        #print(file)
+                        possible_files.append(file)
+                    #Then, cycle through and find the one with the largest number
+                    greatest = '0'
+                    great_path = None
+                    for file in possible_files:
+                        #Very dirty, put should work
+                        fname = os.path.basename(file)
+                        num = []
+                        for char in fname:
+                            try:
+                                int(char) #check if number
+                                num.append(char)
+                                #print('new number ', char)
+                            except:
+                                continue
+                        q = ''.join(num)
+                        #print(q)
+                        if int(q) > int(greatest):
+                            greatest = int(q)
+                            great_path = file
+                    filenames.append(great_path)
+        print(filenames)
+        ans = input('Are these the files you are looking for? ')
+    if ans != 'y':
+        filenames = []
+        print('ok, lets do this differently')
+        print('pick you favorite files from each folder')
+        for name in x:
+            possible_files = []
+            path = os.path.join('Data_Files',name)
+            if os.path.isdir(path):
+                #Check if contains a y
+                if 'z' not in name:
+                    for file in glob.glob(path+"/PCO_Data/*.csv"):
+                        possible_files.append(file)
+                    #Choice which file from the list
+                    print('Directory: ', name)
+                    i = 0
+                    for file in possible_files:
+                        print(i, ' - ', os.path.basename(file))
+                        i += 1
+                    index = input('Choice index of the one you want ')
+                    try:
+                        if index == 'n':
+                            print('skipping directory')
+                        elif 0 <= int(index) <= len(possible_files)-1:
+                            filenames.append(possible_files[int(index)])
+                    except:
+                        print('Bad number, this is the end')
+                        exit()
                     
 
 print(filenames)
