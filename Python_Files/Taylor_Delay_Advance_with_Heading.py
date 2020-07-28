b1d2f1626284ad6fb8aabcb3c3d9f95af687e949
TaylorC20 <tmcor2an@yahoo.com>
2020-07-28 12:43:31 -0400

    New file

---
+++ Python_Files/Taylor_Delay_Advance_with_Heading.py
@@ -0,0 +1,72 @@
+## Imports ##
+
+import time
+import serial
+
+## Globals and Variables ##
+
+global Xbee # Specifies connection to Xbee
+Xbee = serial.Serial('/dev/ttyUSB0', 115200) # Baud rate should be 115200
+#Starting phase angle
+nodephase = int(input("Please enter a starting phase value: "))
+threshold = 360
+
+## Main Code ##
+time1 = time.time()
+time2 = 0
+sendtimemin = 1
+angle = 0 # Actual angle the robot is facing
+heading = 90
+
+while True:
+
+    try:
+        ## Increasing the phase value ##
+        nodephase = heading + (time.time() - time1)
+
+        ## Prints phase at a regular time interval ##
+        if time.time() - time2 > 10:
+            print("{0:.3f}".format(nodephase))
+            time2 += 10
+
+        ## Reaching the Threshold and Sending Pulses ##
+        if nodephase >= threshold:
+            time1 = time.time()
+            message = "T"
+            Xbee.write(message.encode()) #Send the letter over the Xbee
+            print("Pulse Sent")
+            print(nodephase)
+
+        ## Receiving Pulses and Adjusting Phase Value ##
+        if Xbee.inWaiting() > 0: # If there is something in the receive buffer of the Xbee for oscillator 1
+            message = Xbee.read(Xbee.inWaiting()).decode() # Read all data in
+            print(message) # To see what the message is
+            print(nodephase)
+            if 0 < nodephase <= 180:
+                heading -= nodephase/12
+            if 180 < nodephase < threshold:
+                heading += (threshold - nodephase)/12
+
+        ## Heading Cannot be Greater Than 360 ##
+        if heading >= 360:
+            heading = 0
+
+        ## Adjusting Angle to Get Closer to Heading ##
+        if angle != heading:
+            if heading > angle:
+                angle += 5
+            if heading < angle:
+                angle -= 5
+
+    ## Keyboard Interupt ##
+    except KeyboardInterrupt:
+        print('')
+        break
+    
+    ## If the input is not a number ##
+    except type(nodephase) == str:
+        print("Please enter a number.")
+
+## Ending Code ##
+Xbee.close()
+
