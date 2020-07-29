31c546425e376eb246696a683bcbf29bce04fd6e
noahtav <67967658+noahtav@users.noreply.github.com>
2020-07-29 09:06:08 -0400

    Create NoahHeading.py

---
+++ Python_Files/NoahHeading.py
@@ -0,0 +1,53 @@
+import serial
+import time
+import random
+from copy import copy
+
+global Xbee
+Xbee = serial.Serial('/dev/ttyUSB0', 115200)
+
+phase = random.randint(0,360)
+threshold = 360
+previousTime = time.time()
+currentTime = time.time()
+desiredHeading = int(input("Enter desired heading (integers only)."))
+
+while True:
+
+	try:
+	
+		previousTime = copy(currentTime)
+		currentTime = time.time()
+		timeDifference = currentTime - previousTime
+		phase += timeDifference * 60
+
+		if phase >= threshold:
+			phase = 0
+			message = "Received"
+			Xbee.write(message.encode())
+			print("Pulse")
+
+
+		if Xbee.inWaiting() > 0:
+			message = Xbee.read(Xbee.inWaiting()).decode()
+			print(message)
+			if 0 <= phase <= 180:
+				phase -= phase
+			if 180 < phase <= threshold:
+				phase += (threshold - phase)
+
+while True:
+	try:
+		timer = time.time()
+			if time.time - timer > 1.0:
+				print ("{0:.3f}").format(phase)
+				timer += 1.0
+
+		phase = heading + timer.freq
+
+
+	except KeyboardInterrupt:
+		print("Process Interrupted")
+		break
+
+Xbee.close()
