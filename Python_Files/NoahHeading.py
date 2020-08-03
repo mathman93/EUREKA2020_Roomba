854af7eaccb420b220939b56ff6986d2c1ec5fa6
noahtav <67967658+noahtav@users.noreply.github.com>
2020-08-03 02:50:43 -0400

    Update NoahHeading.py

--- Python_Files/NoahHeading.py
+++ Python_Files/NoahHeading.py
@@ -1,3 +1,4 @@
+'''
 import serial
 import time
 import random
@@ -21,13 +22,19 @@ while True:
 
 	try:
 	
-		previousTime = copy(currentTime)
+		
 		currentTime = time.time()
 		timeDifference = currentTime - previousTime
 		phase += timeDifference * 60
 
+		previousTime = copy(currentTime)
+		currentTime = time.time()
+		timer += currentTime - previousTime
+		phase = timer * frequency
+
 		if phase >= threshold:
 			phase = 0
+			timer = cycleTime
 			message = "Received"
 			Xbee.write(message.encode())
 			print("Pulse")
@@ -44,6 +51,55 @@ while True:
 			phase = heading + (timer * frequency)
 			print("The heading is")
 			print(phase)
+
+	except KeyboardInterrupt:
+		print("Process Interrupted")
+		break
+
+Xbee.close()
+'''
+import serial
+import time
+import random
+from copy import copy
+
+global Xbee
+Xbee = serial.Serial('/dev/ttyUSB0', 115200)
+
+phase = random.randint(0,360)
+threshold = 360
+previousTime = time.time()
+currentTime = time.time()
+heading = int(input("Enter desired heading: "))
+frequency = 12
+cycleTime = threshold / frequency
+
+while True:
+
+	try:
+	
+		previousTime = copy(currentTime)
+		currentTime = time.time()
+		timer += currentTime - previousTime
+		timeDifference = currentTime - previousTime
+		phase += heading + (timer * frequency)
+
+		if phase >= threshold:
+			timer = cycleTime
+			phase = 0
+			message = "Received"
+			Xbee.write(message.encode())
+			print("Pulse")
+
+
+		if Xbee.inWaiting() > 0:
+			message = Xbee.read(Xbee.inWaiting()).decode()
+			print(message)
+			if 0 <= phase <= 180:
+				phase -= phase
+			if 180 < phase <= threshold:
+				phase += (threshold - phase)
+
 
 	except KeyboardInterrupt:
 		print("Process Interrupted")
